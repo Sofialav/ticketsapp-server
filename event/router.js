4f0085621a52a3e081798cb8de1f2bba5d304d9f
Sofialav <cemeteryvixen@gmail.com>
2020-03-18 14:58:44 +0100

    get/events date restriction added

--- event/router.js
+++ event/router.js
@@ -1,12 +1,19 @@
 const { Router } = require("express");
 const router = new Router();
 const Event = require("./model");
+const Ticket = require("../ticket/model");
+const Sequelize = require("sequelize");
+const { gt } = Sequelize.Op;
 
 // get events (pagination)
 router.get("/events", (req, res, next) => {
   const limit = Math.min(req.query.limit || 9, 500);
   const offset = req.query.offset || 0;
-  Event.findAndCountAll({ limit, offset })
+  Event.findAndCountAll({
+    where: { end_date: { [gt]: new Date() } },
+    limit,
+    offset
+  })
     .then(result => {
       if (result.count) {
         return res.send({ events: result.rows, total: result.count });
@@ -32,7 +39,9 @@ router.post("/events", async (req, res, next) => {
 // get event by ID
 router.get("/events/:eventId", async (req, res, next) => {
   try {
-    const event = await Event.findByPk(req.params.eventId);
+    const event = await Event.findByPk(req.params.eventId, {
+      include: [Ticket]
+    });
     if (event) {
       return res.status(200).json(event);
     } else {
@@ -42,5 +51,20 @@ router.get("/events/:eventId", async (req, res, next) => {
     next(error);
   }
 });
-
+// get tickets by event ID
+// router.get("/events/:eventId", async (req, res, next) => {
+//   try {
+//     req.params.eventId;
+//     const event = await Ticket.findAll({
+//       where: { date_end: { [gt]: Sequelize.NOW } }
+//     });
+//     if (event) {
+//       return res.status(200).json(event);
+//     } else {
+//       return res.status(404).send("Event does not exist");
+//     }
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 module.exports = router;
