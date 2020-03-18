b5a762b363cced532ea3639e0feb2c69a7c78cae
Sofialav <cemeteryvixen@gmail.com>
2020-03-18 15:20:34 +0100

    post tickets added

--- ticket/router.js
+++ ticket/router.js
@@ -2,4 +2,20 @@ const { Router } = require("express");
 const router = new Router();
 const Ticket = require("./model");
 
+// add ticket
+router.post("/tickets", async (req, res, next) => {
+  // NEED TO ADD WRONG ID VALIDATION
+  try {
+    if (!req.body.price) {
+      return req.status(400).send("Please supply ticket price!");
+    } else if (!req.body.eventId || !req.body.userId) {
+      return res.status(400).send("Please supply event and user ownership");
+    } else {
+      const newTicket = await Ticket.create(req.body);
+      return res.json(newTicket);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = router;
